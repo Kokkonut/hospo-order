756d950e3c62b6930502e9827161b62d0177c3d9
Rickelle <rickelle.1993@live.com.au>
2023-02-14 21:36:30 +1030

    Page/Component started

---
+++ client/src/components/MenuCategory/MenuCategory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MenuCategory() {
+  return (
+    <div>
+        <MenuItem/>
+    </div>
+  )
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import Cart from "../components/Cart";
+// import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  return (
+    <div className="container">
+        <img src={'userIcon.img'} />
+        #infoIcon
+        <Cart />
+        <img href="heroIMAGE">
+        <div>
+            #LOGO
+            #CAFE NAME
+            #CAFE ADDRESS
+        </div>
+        <CategoryMenu />
+            #DROPDOWN-CATEGORY
+      
+    </div>
+  );
+};
+
+export default Home;
