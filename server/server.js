45ff583a929662b9bba0f476e1b504f7a2e10c03
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-14 16:08:56 +1030

    Created server.js
    additional config required for multer, socket.io and stripe

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
+
+};
+
+startApolloServer(typeDefs, resolvers);
+
