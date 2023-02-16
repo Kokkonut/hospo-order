abe7a41e70326cb86dc4fe54becd6a9b7b8be484
Rickelle <rickelle.1993@live.com.au>
2023-02-16 20:36:37 +1030

    LoginPageDrafting

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,57 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+// import Home from './pages/Home';
+import Login from './pages/Login';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
+    <ApolloProvider client={client}>
+    <Router>
+  
     <div className="App">
-     
-      <header className="App-header">
- <h1>hello</h1>
 
+      <header className="App-header">
       </header>
+      <Routes>
+      <Route
+          path="/login"
+          element={<Login />}
+        />
+              {/* <Route 
+                path="/" 
+                element={<Home />} 
+              /> */}
+                  </Routes>
     </div>
+            </Router>
+          </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Login/LoginMain.js
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+
+
+
+function LoginMain() {
+
+  return (
+    <div className="container">
+
+
+      <h2>Login</h2>
+      <form 
+    //   onSubmit={handleFormSubmit}
+      >
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            // onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            // onChange={handleChange}
+          />
+        </div>
+        {/* {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null} */}
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>    
+        <Link to="/signup">← Go back to Signup</Link>
+    </div>
+  );
+}
+
+export default LoginMain;
