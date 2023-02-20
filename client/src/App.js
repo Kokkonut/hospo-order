36eeafa3dc40e60484e90b8bcdc3488b926689e1
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-20 11:22:14 +1030

    1

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,7 @@ import { setContext } from '@apollo/client/link/context';
 
 // import Home from './pages/Home';
 import Login from './pages/Login';
-<<<<<<< HEAD
-import LoginMain from './components/Login/LoginMain';
-import LoginCreate from './components/Login/LoginCreate';
-// import LoginLinks from './components/Login/LoginLinks';
-=======
 import Home from './pages/Home';
->>>>>>> 68c937b369d28774e5fc63ff1e1a6ef64df1eaf4
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -53,25 +47,10 @@ function App() {
           path="/"
           element={<Login />}
         />
-<<<<<<< HEAD
-      <Route
-          path="/login"
-          element={<LoginMain />}
-        />
-      <Route
-          path="/signup"
-          element={<LoginCreate />}
-        />
-              {/* <Route 
-                path="/" 
-                element={<Home />} 
-              /> */}
-=======
       <Route 
            path="/" 
            element={<Home />} 
         />
->>>>>>> 68c937b369d28774e5fc63ff1e1a6ef64df1eaf4
                   </Routes>
     </div>
             </Router>
