5fcca280c711ec24f6a6efd64216c1b63993a15a
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-20 13:36:41 +1030

    installed socket.io and did server config

--- server/server.js
+++ server/server.js
@@ -3,13 +3,16 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// const multer = require('multer');
-// const upload = require('./utils/upload');
+const http = require('http');
+const socketio = require('socket.io');
 const { authMiddleware } = require('./utils/auth');
 const upload = require('./utils/upload');
-const app = express();
+
 const PORT = process.env.PORT || 3003;
 
+const app = express();
+const httpServer = http.createServer(app);
+const io = socketio(httpServer);
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,12 +20,16 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -31,15 +38,10 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  // app.use(upload.single('post_image'));
-
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
-
+  db.once('open', () => {
+    httpServer.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 };
 
 startApolloServer(typeDefs, resolvers);
-
