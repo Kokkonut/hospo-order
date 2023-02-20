dac540c40e6957f12ab3de2778d1dc5583f8b0e8
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-20 14:08:54 +1030

    Server starting with subscription service

--- server/server.js
+++ server/server.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const http = require('http');
+const { execute, subscribe } = require('graphql');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const http = require('http');
-const socketio = require('socket.io');
 const { authMiddleware } = require('./utils/auth');
 const upload = require('./utils/upload');
+
+// const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 3003;
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = socketio(httpServer);
+// const io = socketio(httpServer);
+
+// web socket server
+const wsServer = new SubscriptionServer(
+  {
+    execute,
+    subscribe,
+    schema: new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware,
+    }).schema,
+  },
+  {
+    server: httpServer,
+    path: '/subscriptions',
+  }
+);
 
 const server = new ApolloServer({
   typeDefs,
@@ -23,12 +43,12 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
+// io.on('connection', (socket) => {
+//   console.log('New client connected');
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -41,6 +61,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     httpServer.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use subscriptions at ws://localhost:${PORT}${wsServer.subscriptionsPath}`);
   });
 };
 
