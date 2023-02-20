826e1851d44db9471e7c580433560d52acfd2657
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-20 11:54:13 +1030

    Update: order model;

--- server/models/orders.js
+++ server/models/orders.js
@@ -1,40 +1,20 @@
-const { Schema, model } = require('mongoose');
-const orderSchema = new Schema({
-    items: [{
-      menuItem: {
-        type: Schema.Types.ObjectId,
-        ref: 'MenuItem',
-        required: true
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        min: 1
-      },
-      notes: {
-        type: String,
-        trim: true
-      }
-    }],
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    status: {
-      type: String,
-      enum: ['New', 'In Progress', 'Ready', 'Completed', 'Canceled'],
-      default: 'New'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-  }, {
-    timestamps: true
-  });
+const mongoose = require('mongoose');
 
-const Order = model('Order', orderSchema);
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ]
+});
+
+const Order = mongoose.model('Order', orderSchema);
 
 module.exports = Order;
-  
