3da6b8be3c1c706101d3b4a7e5eac5df83d31636
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-21 16:44:20 +1030

    moved menu items to their own comonent

--- client/src/components/Menu/MenuCategory.js
+++ client/src/components/Menu/MenuCategory.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_PRODUCTS } from '../../utils/queries';
+import MenuItem from './MenuItem';
 
 const MenuCategory = () => {
-  // Fetch all products from the GraphQL API
   const { loading, error, data } = useQuery(QUERY_ALL_PRODUCTS);
-
-  // Declare state to store categories and initialize it as an empty array
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    // If the data has been fetched, update the categories state
     if (data) {
-      // Group the products by category
       const productsByCategory = data.products.reduce((accumulator, product) => {
         const { category } = product;
         if (!accumulator[category._id]) {
@@ -21,8 +17,6 @@ const MenuCategory = () => {
         accumulator[category._id].products.push(product);
         return accumulator;
       }, {});
-
-      // Convert the object of categories to an array and update the state
       setCategories(Object.values(productsByCategory));
     }
   }, [data]);
@@ -37,19 +31,11 @@ const MenuCategory = () => {
 
   return (
     <div>
-      {/* Map over each category and its products */}
       {categories.map(category => (
         <div key={category._id}>
-          {/* Display the category name */}
           <h2>{category.name}</h2>
-          {/* Map over each product in the category and display its details */}
           {category.products.map(product => (
-            <div key={product._id}>
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>Price: ${product.price.toFixed(2)}</p>
-              <p>Quantity: {product.quantity}</p>
-            </div>
+            <MenuItem key={product._id} product={product} />
           ))}
         </div>
       ))}
@@ -58,3 +44,4 @@ const MenuCategory = () => {
 };
 
 export default MenuCategory;
+
